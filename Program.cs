85fb22f32fdeab53ed9d50dd3e57dbf89dee4ca3
AliceTamara <alice_tamara@hotmail.com>
2022-06-08 13:00:29 -0300

    Exercícios da Aula 2

--- Program.cs
+++ Program.cs
@@ -6,103 +6,102 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            //exercicio1
-            Console.WriteLine("-----Exercicio 1=====");
-            string nome = "Alice Tamara Suyama de Santana";
-            string endereco = "Rua Maracujá";
-            int numeroResidencia = 70;
-            int cep = 45312125;
-            var telefone = 11988888585;
-
-            Console.WriteLine($"Meu nome completo é: {nome} \nMeu endereço é: {endereco}, {numeroResidencia} \nCEP: {cep} \nMeu Telefone é {telefone}");
-
-            //exercicio2
-            Console.WriteLine("\n\n-----Exercicio 2=====");
-            string mulherFamosaTecnologia = "Carol Shaw";
-            string formacao = "Ciencias da Computação";
-            string contribuicao = " foi uma das primeiras mulheres designers e programadoras de jogos na indústria de videogames." +
-                "Ela foi capaz de pavimentar o caminho para futuras mulheres no jogo.";
-
-            Console.WriteLine($"A mulher famosa dentro da tecnologia que escolhi foi: {mulherFamosaTecnologia} \nSua formação é: {formacao} " +
-                $"\nsua contribuição dentro da tecnologia foi: {contribuicao}");
-
-            //exercicio3
-            Console.WriteLine("\n\n-----Exercicio 3-====");
-
-            string musica = "Chão de Giz";
-            string compositor = "Zé Ramalho";
-            string genero = "MPB";
-            string letraDaMusica = "Eu desço dessa solidão" +
-                "\nEspalho coisas sobre um chão de giz" +
-                "\n" +
-                "Há meros devaneios tolos a me torturar" +
-                "\n" +
-                "Fotografias recortadas" +
-                "\n" +
-                "Em jornais de folhas amiúde" +
-                "\n" +
-                "Eu vou te jogar num pano de guardar confetes" +
-                "\n" +
-                "Eu vou te jogar num pano de guardar confetes" +
-                "\n" +
-                "Disparo balas de canhão" +
-                "\n" +
-                "É inútil, pois existe um grão-vizir" +
-                "\n" +
-                "Há tantas violetas velhas sem um colibri" +
-                "\n" +
-                "Queria usar, quem sabe" +
-                "\n" +
-                "Uma camisa de força ou de Vênus" +
-                "\n" +
-                "Mas não vou gozar de nós apenas um cigarro" +
-                "\n" +
-                "Nem vou lhe beijar, gastando assim o meu batom" +
-                "\n" +
-                "Agora pego um caminhão" +
-                "\n" +
-                "Na lona vou a nocaute outra vez" +
-                "\n" +
-                "Pra sempre fui acorrentado no seu calcanhar" +
-                "\n" +
-                "Meus vinte anos de boy, that's over, baby" +
-                "\n" +
-                "Freud explica" +
-                "\n" +
-                "Não vou me sujar fumando apenas um cigarro" +
-                "\n" +
-                "Nem vou lhe beijar, gastando assim o meu batom" +
-                "\n" +
-                "Quanto ao pano dos confetes, já passou meu carnaval" +
-                "\n" +
-                "E isso explica porque o sexo é assunto popular" +
-                "\n" +
-                "No mais, estou indo embora" +
-                "\n" +
-                "No mais, estou indo embora" +
-                "\n" +
-                "No mais, estou indo embora" +
-                "\n" +
-                "No mais";
-
-            Console.WriteLine($"Música: {musica}\n\nLetra: {letraDaMusica}\n\nCompositor: {compositor}\nGenero Musical: {genero}");
-
-            //exercicio4
-            Console.WriteLine("\n\n-----Exercicio 4-====");
-
-            Console.WriteLine("Digite seu Nome: ");
-            string nome1 = Console.ReadLine();
-            var mensagem = "Em qualquer area teremos desafios, mas o maior desafio é vencer as barreiras \nque nós mesmas construimos em nossa mente, então seja corajosa!!!" +
-                "\nnão desista de você e do que você acredita, lembre-se sempre de tudo que já enfrentou para estar aqui\ne use tudo isso como degraus pra te empurrar" +
-                "\npra cima toda vez que pensar em desistir. \nO medo vem junto, mas você é MULHER! \nVai com medo e sorrindo!";
-
-            Console.WriteLine($"Olá {nome1}\n{mensagem}");
-
-
+            Exercicio1();
+            Exercicio2();
+            Exercicio3();
+            Exercicio4();
+            Exercicio5();
         }
 
+        static void Exercicio1()
+        {
+            Console.WriteLine("-----Exercicio 1-----");
+
+            int anoDeNascimento = 0;
+            Console.WriteLine("Digite seu ano de nascimento(deverá conter 4 digitos): ");
+            anoDeNascimento = Convert.ToInt32(Console.ReadLine());
+
+            var anoAtual = DateTime.Today.Year;
+            var adicao = anoAtual - anoDeNascimento;
+
+            Console.WriteLine($"O ano de nascimento digitado foi {anoDeNascimento} sua idade é {adicao}");
+        }
+
+        static void Exercicio2()
+        {
+            Console.WriteLine("-----Exercicio 2-----");
+
+            Console.WriteLine("Digite 10 numeros inteiros: ");
+            var numeros = new int[10];
+            var soma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                soma = soma + numeros[i];
+            }
+            Console.WriteLine($"A soma dos numeros digitados é: {soma}");
+        }
+
+        static void Exercicio3()
+        {
+            Console.WriteLine("-----Exercicio 3-----");
+
+            Console.WriteLine("Nome do Funcionário: ");
+            var funcionario = Console.ReadLine();
+
+            Console.WriteLine("Digite as horas trabalhadas: ");
+            var horasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Valor que Recebe por hora: ");
+            var valorPorHora = Convert.ToDouble(Console.ReadLine());
+
+            var multiplicacaoDeHorasTrabalhadasPorValor = horasTrabalhadas * valorPorHora;
+
+            Console.WriteLine($"O funcionário {funcionario}, trabalhou {horasTrabalhadas.ToString("F2")} e tem à receber {multiplicacaoDeHorasTrabalhadasPorValor.ToString("F2")}");
+        }
+
+        static void Exercicio4()
+        {
+            Console.WriteLine("-----Exercicio 4-----");
+            Console.WriteLine("Digite sua idade:");
+            var idade = Convert.ToInt32(Console.ReadLine());
+
+            var calculoEmDias = idade * 365;
+            var calculoEmMeses = idade * 12;
+
+            Console.WriteLine($"Sua idade é {idade}, você tem o total de {calculoEmDias} dias vividos e {calculoEmMeses} meses vividos.");
+        }
+
+        static void Exercicio5()
+        {
+            Console.WriteLine("-----Exercicio 5-----");
+
+            Console.WriteLine("Digite o valor em Real: ");
+            var valorRecebidoEmReal = Convert.ToDouble(Console.ReadLine());
+
+            var valorDolar = 4.86;
+            var valorEuro = 5.20;
+            var valorLibraEsterlina = 6.12;
+            var valorDolarCanadense = 3.88;
+            var valorPesoArgentino = 0.040;
+            var valorPesoChileno = 0.0059;
+
+
+            var realParaDolar = valorRecebidoEmReal / valorDolar;
+            var realParaEuro = valorRecebidoEmReal / valorEuro;
+            var realParaLibraEsterlina = valorRecebidoEmReal / valorLibraEsterlina;
+            var realParaDolarCanadense = valorRecebidoEmReal / valorDolarCanadense;
+            var realParaPesoArgentino = valorRecebidoEmReal / valorPesoArgentino;
+            var realParaPesoChileno = valorRecebidoEmReal / valorPesoChileno;
+
+            Console.WriteLine($"O Valor R${valorRecebidoEmReal} equivale a: " +
+                $"\n{realParaDolar} Dólares" +
+                $"\n{realParaEuro} Euros" +
+                $"\n{realParaLibraEsterlina} Libras Esterlinas" +
+                $"\n{realParaDolarCanadense} Dolares Canadenses" +
+                $"\n{realParaPesoArgentino} Pesos Argentinos" +
+                $"\n{realParaPesoChileno} Pesos Chilenos");
+        }
     }
 }
-
-
-
