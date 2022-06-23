85d89b082bcdb54793b799b72540d479e0191c0b
AliceTamara <alice_tamara@hotmail.com>
2022-06-23 15:32:37 -0300

    Aula 8

---
+++ Jogador.cs
@@ -0,0 +1,72 @@
+﻿namespace CodingGirlsProject
+{
+    internal class Jogador
+    {
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public int Numero { get; set; }
+        public double PontosDeHabilidade { get; set; }
+        public string Cartoes { get; set; }
+        public bool Suspenso { get; set; }
+        public string Posicao { get; set; }
+
+        public Jogador(string nome, string apelido, int numero, double pontosDeHabilidade, 
+            string cartoes, bool suspenso, string posicao)
+        {
+            Nome = nome;
+            Apelido = apelido;
+            Numero = numero;
+
+            if (pontosDeHabilidade > 100)
+                throw new Exception("Pontos de habilidade nao podem ser maior que 100.");
+
+            PontosDeHabilidade = pontosDeHabilidade;
+            Cartoes = cartoes;
+            Suspenso = suspenso;
+            Posicao = posicao;
+        }
+        
+        public bool EstaApto() =>
+            !Suspenso;
+
+        public void TornarJogadorSuspenso()
+        {
+            if (EstaApto())
+                Suspenso = true;
+        }
+
+        public void CumprirSuspensao()
+        {
+            if (Suspenso)
+                Suspenso = false;
+        }
+
+        public void SofrerLesao()
+        {
+            var lesoes = new Dictionary<string, double>
+            {
+                {"Dor de Cabeca", 1 },
+                {"Torcicolo", 2 },
+                {"Distensao Muscular", 3 },
+                {"Dor na mão", 4 },
+                {"Estiramento", 5 },
+                {"Torcao", 10 },
+                {"Braco Quebrado", 15}
+            };
+            
+            var rdm = new Random().Next(0, lesoes.Count - 1);
+            var lesoesKeys = lesoes.Keys.ToArray();
+            var lesaoKeyRandom = lesoesKeys[rdm];
+            
+            PontosDeHabilidade -= lesoes[lesaoKeyRandom];
+        }
+
+        public void Treinar()
+        {
+            if(PontosDeHabilidade == 100)
+                throw new Exception("Pontos de habilidade nao podem ser maior que 100.");
+
+            PontosDeHabilidade++;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,141 +8,99 @@ namespace Program
         {
             Exercicio1();
             //Exercicio2();
-            Exercicio3();
-            Exercicio4();
-            Exercicio5();
+            //Exercicio3();
+            //Exercicio4();
+            //Exercicio5();
         }
 
         static void Exercicio1()
         {
             Console.WriteLine("-----Exercicio 1-----");
-            var tamara = new Aluno(1, "Alice", 100);
-            var filipe = new Aluno(2, "Filipe", 100);
 
-            filipe.AdicionarNotas(new double[] {10, 10, 10, 10});
-            tamara.AdicionarNotas(new double[] {10, 10, 10, 10});
-            
-            tamara.Cadastrar(filipe);
+            var nome = "Gilberto";
+            var apelido = "Giba";
+            var numero = 10;
+            var pontosDeHabilidade = 90;
+            var cartoes = string.Empty;
+            var suspenso = false;
+            var posicao = "Atacante";
 
-            var consultaFilipe = tamara.ConsultarPorCodigo(2);
-            var consultaTodos = tamara.Consultar();
+            var jogador = new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao);
+
+            jogador.EstaApto();
+            jogador.TornarJogadorSuspenso();
+            jogador.CumprirSuspensao();
+            jogador.SofrerLesao();
+
+            var nomeTimeCasa= "Brasil";
+            var fundacaoTimeCasa = "bradesco";
+            var jogadoresTimeCasa = new List<Jogador>()
+            {
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao)
+            };
+
+            var timeCasa = new Time(nomeTimeCasa, fundacaoTimeCasa, jogadoresTimeCasa);
+            timeCasa.ImprimirEscala();
+
+            var nomeTimeVisitante = "Chile";
+            var fundacaoTimeVisitante = "Santander";
+            var jogadoresTimeVisitante = new List<Jogador>()
+            {
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao),
+                new Jogador(nome, apelido, numero, pontosDeHabilidade, cartoes, suspenso, posicao)
+            };
+
+            var timeVisitante = new Time(nomeTimeVisitante, fundacaoTimeVisitante, jogadoresTimeVisitante);
+            timeVisitante.ImprimirEscala();
+
+            var dataDoJogo = DateTime.Now;
+            var cidade = "São Bernardo do Campo";
+            var placarTimeDaCasa = 10;
+            var placarTimeVisitante = 8;
+
+            var jogo = new Jogo(timeCasa, timeVisitante, dataDoJogo, cidade, placarTimeDaCasa, placarTimeVisitante);
+
+            jogo.VerificarEquipeComMaisChanceDeVitoria();
+            jogo.GerarLesaoAleatoriaEmAlgumJogador();
+            jogo.TerminarPartida();
+            jogo.PermitirTreinoAposTermino();
         }
 
         static void Exercicio2()
         {
-
-            
+            Console.WriteLine("-----Exercicio 2-----");
         }
 
         static void Exercicio3()
         {
             Console.WriteLine("-----Exercicio 3-----");
-
-            Console.WriteLine("Digite seu nome completo: ");
-            var titular = Console.ReadLine();
-
-            Console.WriteLine("Digite o numero da conta: ");
-            var conta = Convert.ToInt32(Console.ReadLine());
-
-            var contaBancaria = new ContaBancaria(titular, conta);
-
-            Console.WriteLine("Gostaria de alterar o titular da conta? S - para sim e N - para não)");
-            var respostaAlteracaoTitular = Console.ReadLine();
-
-            if (respostaAlteracaoTitular.ToUpper().FirstOrDefault() == 'S')
-            {
-                Console.WriteLine("Insira o novo nome do titular: ");
-                var novoTitular = Console.ReadLine();
-                contaBancaria.AlterarTitular(novoTitular);
-            }
-
-            Console.WriteLine("Gostaria de realizar um depósito? S - para sim e N - para não)");
-            var respostaDeposito = Console.ReadLine();
-
-            if (respostaDeposito.ToUpper().FirstOrDefault() == 'S')
-            {
-                Console.WriteLine("Insira o valor do depósito: ");
-                var novoDeposito = Convert.ToDouble(Console.ReadLine());
-                contaBancaria.Deposito(novoDeposito);
-            }
-
-            Console.WriteLine("Gostaria de realizar um saque? S - para sim e N - para não)");
-            var respostaSaque = Console.ReadLine();
-
-            if (respostaSaque.ToUpper().FirstOrDefault() == 'S')
-            {
-                Console.WriteLine("Insira o valor do saque: ");
-                var novoSaque = Convert.ToDouble(Console.ReadLine());
-                contaBancaria.Saque(novoSaque);
-            }
         }
 
         static void Exercicio4()
         {
             Console.WriteLine("-----Exercicio 4-----");
-
-            Console.WriteLine("Cadastro do seu humano: ");
-
-            Console.WriteLine("Digite o nome: ");
-            var nomeDono = Console.ReadLine();
-
-            Console.WriteLine("Digite o telefone: ");
-            var telefone = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Digite o endereço: ");
-            var endereco = Console.ReadLine();
-
-            var dono = new Dono(nomeDono, telefone, endereco);
-
-            Console.WriteLine("Cadastro PET: ");
-            Console.WriteLine("Digite o nome do seu pet: ");
-            var nomeAnimal = Console.ReadLine();
-
-            Console.WriteLine("Digite a especie: ");
-            var especie = Console.ReadLine();
-
-            Console.WriteLine("Dgite a cor: ");
-            var cor = Console.ReadLine();
-
-            Console.WriteLine("Digite a idade: ");
-            var idadeAnimal = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Alguma observação? Digite aqui: ");
-            var observacoes = Console.ReadLine();
-
-            var animal = new Animal(nomeAnimal, especie, cor,
-            idadeAnimal, observacoes, dono);
         }
 
         static void Exercicio5()
         {
             Console.WriteLine("-----Exercicio 5-----");
-
-            var canais = new List<Canal>()
-            {
-                new Canal(1, "globo"),
-                new Canal(2, "sbt"),
-                new Canal(3, "band"),
-                new Canal(4, "cultura"),
-                new Canal(5, "record"),
-                new Canal(6, "rede vida"),
-                new Canal(7, "polishop")
-            };
-
-            string marca = "Samsung";
-            int codigoDeSerie = 123456789;
-            
-            var tv = new TV(marca, codigoDeSerie, canais);
-
-            Console.WriteLine($"A TV estava {tv.Status}");
-            tv.LigarTV();
-            Console.WriteLine($"A TV esta agora {tv.Status}");
-
-            tv.AumentarVolume();
-            tv.DiminuirVolume();
-            tv.MudarCanal(2);
-
-            Console.WriteLine($"O canal atual é {tv.CanalAtual.Nome}");
         }
     }
 }
